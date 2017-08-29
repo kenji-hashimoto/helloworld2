2829f2d7163ae7917891bedf139797abfb161b5e
DESKTOP-E8KGCP5\Kenji Hashimoto <kenji.hashimoto@cybernauts.co.jp>
2017-08-29 21:32:43 +0900

    aaa

--- main/java/com/example/javamavenjunithelloworld/HelloApp.java
+++ main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -9,7 +9,7 @@ public class HelloApp {
     static int DEFAULT_TIMES = 3;
 
     static int EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD = 2;
-    static int EXIT_STATUS_HELLO_FAILED = 4;
+    static int EXIT_STATUS_HELLO_FAILED = 5;
 
     /**
      * The main method of this program.
